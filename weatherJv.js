5a8b31464f22fa3612225ccbae28aa8feaa878ab
mahoro-belyse <151196672+mahoro-belyse@users.noreply.github.com>
2023-12-10 15:46:04 +0200

    added same jv

--- weatherJv.js
+++ weatherJv.js
@@ -1,1 +1,8 @@
-alert("hello");
+function search(event) {
+  event.preventDefault();
+  let searchbar = document.querySelector("#SearchInput");
+  let cityelement = document.querySelector("#city");
+  cityelement.innerHTML = searchbar.value;
+}
+let searchinput = document.querySelector("#searchform");
+searchinput.addEventListener("submit", search);
